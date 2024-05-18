e2f70f8acd14a454b7eca408c867d23fad785b75
Kuat Tulegenov <29144@iitu.edu.kz>
2024-05-18 20:51:10 +0500

    Test Kuka app.py

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__, template_folder='html/')
 def home():
 	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	return render_template(aboutus.html)
+
 if __name__ == '__main__':
 	app.run(debug = True, host='0.0.0.0', port=5000)
