f3c35b7f8227309bdce3ba0a38bec86ba748f43c
Kuat Tulegenov <29144@iitu.edu.kz>
2024-05-19 16:13:12 +0500

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-import model
 
 app = Flask(__name__, template_folder='html/')
 
 @app.route('/')
-
 def home():
 	return render_template('index.html')
 
@@ -13,6 +11,8 @@ def about():
 	return render_template('aboutus.html')
 
 @app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
 	app.run(debug = True, host='0.0.0.0', port=5000)
