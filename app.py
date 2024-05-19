5d2a77d984d8832f1f623c0855320cd5cb7b564c
Kuat Tulegenov <29144@iitu.edu.kz>
2024-05-19 16:46:55 +0500

    Получилось урааа

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__, template_folder='html/')
+app = Flask(__name__, template_folder='html/', static_folder='.', static_url_path='')
 
 @app.route('/')
 def home():
 	return render_template('index.html')
 
-@app.route('/about')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/aboutus.html')
 def about():
 	return render_template('aboutus.html')
 
-@app.route('/contact')
+@app.route('/contact.html')
 def contact():
     return render_template('contact.html')
 
